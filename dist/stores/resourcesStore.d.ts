694ce286dff3fce025cecffb108ea48c22a4ea8e
0x0xu <peter.chochula@notum.cz>
2023-03-08 13:35:58 +0100

    build

--- dist/stores/resourcesStore.d.ts
+++
@@ -1,7 +0,0 @@
-declare class ResourcesStore {
-    dataPromises: Array<Promise<any>>;
-    addPromise(newPromise: Promise<any>): void;
-    resolveAll(): Promise<any[]>;
-}
-export declare const resourcesStore: ResourcesStore;
-export {};
